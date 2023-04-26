647ba75a592b38e9346b537e70d796ede575c225
SvetaLantsman <svetlana@curalife.com>
2023-04-26 10:47:00 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Costs from "./components/Cost/Costs";
 
 function App() {
-  
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Refrigerator",
+      amount: 999.99
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: "Jeans",
+      amount: 49.99
+    }
+  ]
   return (
     <div>
       <h1>Start</h1>
-       <Costs/>
+       <Costs costs={costs}/>
     </div>
   );
 }
--- src/components/Cost/Costs.js
+++ src/components/Cost/Costs.js
@@ -1,29 +1,12 @@
 import './Costs.css';
 import CostItem from './CostItem';
-function Costs (){
-    const costs = [
-      {
-        date: new Date(2021, 2, 12),
-        description: "Refrigerator",
-        amount: 999.99
-      },
-      {
-        date: new Date(2021, 11, 25),
-        description: "MacBook",
-        amount: 1254.72
-      },
-      {
-        date: new Date(2021, 3, 1),
-        description: "Jeans",
-        amount: 49.99
-      }
-    ]
-
+function Costs (props){
+   
     return (
       <div className='costs'>
-        <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
-        <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-        <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+        <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
+        <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
+        <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
       </div>
     )
 }
