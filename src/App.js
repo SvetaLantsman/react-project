6ed1afda0c1a77d87f16d85e40fe9ff718c1c703
SvetaLantsman <svetlana@curalife.com>
2023-04-26 10:11:34 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import CostItem from "./components/Cost/CostItem";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Refrigerator",
+      amount: 999.99
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72
+    },
+    {
+      date: new Date(2021, 4, 1),
+      description: "Jeans",
+      amount: 49.99
+    }
+  ]
   return (
     <div>
       <h1>Start</h1>
-    <CostItem/>
+      <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}></CostItem>
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}></CostItem>
+      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}></CostItem>
     </div>
   );
 }
