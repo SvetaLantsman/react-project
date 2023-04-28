c117368c7b39010ca8a7d9f77cca6b15f607d715
SvetaLantsman <svetlana@curalife.com>
2023-04-28 10:55:05 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import Costs from "./components/Cost/Costs";
 import NewCost from "./components/NewCost/NewCost";
-const App = () => {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2021, 2, 12),
-      description: "Refrigerator",
-      amount: 999.99
-    },
-    {
-      id: "c2",
-      date: new Date(2021, 11, 25),
-      description: "MacBook",
-      amount: 1254.72
-    },
-    {
-      id: "c3",
-      date: new Date(2021, 3, 1),
-      description: "Jeans",
-      amount: 49.99
-    }
-  ]
+import React, {useState} from "react";
 
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2021, 2, 12),
+    description: "Refrigerator",
+    amount: 999.99
+  },
+  {
+    id: "c2",
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 3, 1),
+    description: "Jeans",
+    amount: 49.99
+  }
+]
+const App = () => {
+ 
+  const [costs, setCosts]= useState(INITIAL_COSTS)
   const addCostHandler =(cost)=>{
-    console.log('App: ',cost);
-    console.log('App Component')
+    setCosts(prevCosts=>{
+      return [cost, ...prevCosts];
+      
+    });
+    
   }
 
   // return React.createElement(
--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -44,7 +44,7 @@ const CostForm = (props)=>{
    const submitHandler =(event)=>{
         event.preventDefault();
         const costData = {
-            name: inputName,
+            description: inputName,
             amount: inputAmount,
             date: new Date(inputDate)
         };
