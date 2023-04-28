6b48b78f2e75bb6eb7e56f02aca63d23d94e0925
SvetaLantsman <svetlana@curalife.com>
2023-04-28 16:21:43 +0300

    .

--- src/components/Cost/Costs.js
+++ src/components/Cost/Costs.js
@@ -4,29 +4,34 @@ import Card from '../UI/Card';
 import CostsFilter from './CostsFilter';
 import React, {useState} from 'react';
 const Costs = (props)=>{
-  const [selectedYear, setSelectedYear] = useState('2019');
+  const [selectedYear, setSelectedYear] = useState('2022');
    const yearChangeHandler =(year)=>{
       setSelectedYear(year);
    }
+
+   
 
    const filterdCosts = props.costs.filter(
     cost=>{
       return cost.date.getFullYear().toString()=== selectedYear;
     }
    )
+
+   let costContent = <p>No costs in this year</p>;
+
+   if(filterdCosts.length>0){
+    costContent = filterdCosts.map(cost =>
+    (<CostItem 
+      key ={cost.id}
+      date={cost.date} 
+      description={cost.description} 
+      amount={cost.amount}/>))
+   }
     return (
       <div> 
       <Card className='costs'>
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler}/>
-        {filterdCosts.length === 0 && <p>No costs in this year</p>}
-        {filterdCosts.length > 0 && filterdCosts.map(cost =>
-        (<CostItem 
-          key ={cost.id}
-          date={cost.date} 
-          description={cost.description} 
-          amount={cost.amount}/>))
-        }
-        
+        {costContent}
       </Card>
       </div>
     )
